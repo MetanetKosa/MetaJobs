806d2379c025bca91e7833f32fd9610a8c06752a
ssohee98 <jsohee98@naver.com>
2023-03-13 17:37:11 +0900

    fix: modify

--- src/main/java/org/recruit/login/domain/CompanyVO.java
+++ src/main/java/org/recruit/login/domain/CompanyVO.java
@@ -7,18 +7,18 @@ import lombok.Data;
 @Data
 public class CompanyVO {
 
-	private String id;
-	private String pw;
-	private String auth;
-	private String cno;
-	private String name;
-	private String phone;
-	private String address;
-	private String type;
-	private String manager;
-	private Date joinDate;
-	private Date updateDate;
-	private int enabled;
+	private String com_id;
+	private String com_pw;
+	private String com_auth;
+	private String com_cno;
+	private String com_name;
+	private String com_phone;
+	private String com_address;
+	private String com_type;
+	private String com_ceo;
+	private Date com_joinDate;
+	private Date com_updateDate;
+	private int com_enabled;
 	
 	//한 멤버에 여러 auth를 가질 수 있으므로 list collection을 만들기
 	//private List<AuthVO> authList;
--- src/main/java/org/recruit/login/domain/MemberVO.java
+++ src/main/java/org/recruit/login/domain/MemberVO.java
@@ -9,20 +9,20 @@ import lombok.Data;
 @Data
 public class MemberVO {
 
-	private Long mno;
-	private String id;
-	private String pw;
-	private String auth;
-	private String name;
+	private Long mem_no;
+	private String mem_id;
+	private String mem_pw;
+	private String mem_auth;
+	private String mem_name;
 	//날짜형 입력받을시 문자형으로 들어오므로 패턴 지정해서 Date객체 만들기 400error
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date birth;
-	private String phone;
-	private String email;
-	private String address;
-	private Date joinDate;
-	private Date updateDate;
-	private int enabled;
+	private Date mem_birth;
+	private String mem_phone;
+	private String mem_email;
+	private String mem_address;
+	private Date mem_joinDate;
+	private Date mem_updateDate;
+	private int mem_enabled;
 	
 	//한 멤버에 여러 auth를 가질 수 있으므로 list collection을 만들기
 	//private List<AuthVO> authList;
